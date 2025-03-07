e481b1fb72575a3b0f449e8b14626ea517b0491f
Attissou3003 <valeriekane3003@gmail.com>
2025-03-07 12:07:03 +0100

    Update master.sh
    changer quelques informations

--- scripts/master.sh
+++ scripts/master.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 
 # If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
-PUBLIC_IP_ACCESS="false"
+PUBLIC_IP_ACCESS="true"
 NODENAME=$(hostname -s)
 POD_CIDR="192.168.0.0/16"
 
@@ -17,9 +17,9 @@ sudo kubeadm config images pull
 
 # Initialize kubeadm based on PUBLIC_IP_ACCESS
 
-if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
+if [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
     
-    MASTER_PRIVATE_IP=$(ip addr show eth1 | awk '/inet / {print $2}' | cut -d/ -f1)
+    MASTER_PRIVATE_IP=$(ip addr show ens33 | awk '/inet / {print $2}' | cut -d/ -f1)
     sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
 elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
