a57aa4b6b3ef6ebfb6a9815fe2933d0de077d153
Attissou3003 <valeriekane3003@gmail.com>
2025-04-11 14:39:43 +0200

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -5,7 +5,7 @@
 set -euxo pipefail
 
 # If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
-CONTROL_PLANE_ENDPOINT="192.168.1.100:6443"  # IP/nom DNS du load balancer (obligatoire pour HA)
+
 PUBLIC_IP_ACCESS="false"
 NODENAME=$(hostname -s)
 POD_CIDR="192.168.0.0/16"
@@ -19,9 +19,9 @@ sudo kubeadm config images pull
 if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
     MASTER_PRIVATE_IP=$(ip addr show eth1 | awk '/inet / {print $2}' | cut -d/ -f1)
-    sudo kubeadm init --control-plane-endpoint="$CONTROL_PLANE_ENDPOINT" --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
-elif [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
+elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
 
     MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
     sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
@@ -40,9 +40,3 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 # Install Claico Network Plugin Network 
 
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-
-# Afficher les commandes kubeadm join (pour master2)
-echo "======= Commande kubeadm join pour les autres masters ======="
-kubeadm token create --print-join-command --ttl 1h
-CERT_KEY=$(kubeadm init phase upload-certs --upload-certs | tail -1)
-echo "→ Certificate key à utiliser : $CERT_KEY"
